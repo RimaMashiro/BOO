3e4fb64e71b86621d785b8cb550a7ded00990004
Mashiro Rima <vladlenakarimova1@gmail.com>
2023-04-18 21:29:35 +0500

    Initial commit

---
+++ app/src/main/java/com/example/boo/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.boo;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+        new DownloadImageTask((ImageView) findViewById(R.id.imageView))
+                .execute("https://png.pngtree.com/png-vector/20220728/ourmid/pngtree-celebration-background-with-confetti-and-gold-ribbons-overlay-png-vector-illustration-png-image_6089804.png");
+    }
+
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Ошибка передачи изображения", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+
+}
